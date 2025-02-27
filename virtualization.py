1bb5daf474f722746e97f211d4b24469b144ea79
Gabriel Gaytan III <Rvayatan@gmail.com>
2025-02-27 16:45:16 -0700

    virtualization.py

--- virtualization.py
+++ virtualization.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("/Users/gabrielgaytaniii/Desktop/codes/Projects/Python Workshop/dta.csv")
+df = pd.read_csv("dta.csv")
 
 plt.figure(figsize=(10, 5))
 plt.bar(df["Country"], df["Emissions"], color="blue")
